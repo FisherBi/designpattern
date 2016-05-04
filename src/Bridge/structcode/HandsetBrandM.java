b03569cf7d80bb13fdca1f7b5b93eb96c86d64e8
fisbii <fisher.bi@leandev.se>
2016-05-04 17:50:24 +0800

    commit Bridge pattern.

---
+++ src/Bridge/structcode/HandsetBrandM.java
@@ -0,0 +1,11 @@
+package Bridge.structcode;
+
+/**
+ * Created by fisbii on 16-5-4.
+ */
+public class HandsetBrandM extends HandsetBrand {
+    @Override
+    public void run() {
+        soft.run();
+    }
+}
---
+++ src/Bridge/structcode/HandsetGame.java
@@ -0,0 +1,13 @@
+package Bridge.structcode;
+
+/**
+ * Created by fisbii on 16-5-4.
+ */
+
+//手机游戏
+public class HandsetGame extends HandsetSoft {
+    @Override
+    public void run() {
+        System.out.println("运行手机游戏");
+    }
+}

cd56647bb80f48b16adf64e22e66c21aca593886
fisbii <fisher.bi@leandev.se>
2016-05-18 17:02:51 +0800

    commit interpreter pattern.

---
+++ src/interpreter/structcode/Expression.java
@@ -0,0 +1,23 @@
+package interpreter.structcode;
+
+/**
+ * Created by fisbii on 16-5-18.
+ */
+//表达式类
+public abstract class Expression {
+    //解释器
+    public void interpret(PlayContext context){
+        if(context.getText().length() == 0){
+            return;
+        }else{
+            String playKey = context.getText().substring(0,1);
+            context.setText(context.getText().substring(2));
+            double playValue = Double.parseDouble(context.getText().substring(0,context.getText().indexOf(" ")));
+            context.setText(context.getText().substring(context.getText().indexOf(" ") + 1));
+            execute(playKey,playValue);
+        }
+    }
+
+    //执行
+    public abstract void execute(String key,double value);
+}

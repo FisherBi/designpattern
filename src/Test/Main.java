b65a1d0163d2adc92da1f503a7435276d8a38bd5
fisbii <fisher.bi@leandev.se>
2016-06-14 16:38:04 +0800

    test code.

--- src/Test/Main.java
+++ src/Test/Main.java
@@ -1,10 +1,25 @@
 package Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /**
  * Created by fisbii on 16-3-24.
  */
 public class Main {
     public static void main(String args[]){
-        Person person = new Person();
+        File file = new File("/data/home/fisbii/dockerconf/backend/audit/key/rsa_greenback.public");
+        System.out.println(file.getAbsolutePath());
+        System.out.println(file.getName());
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(file.getName(),true);
+            fileOutputStream.write("aaabbb".getBytes());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
